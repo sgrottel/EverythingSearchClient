188907e33651e7f0052aa23a03f2b5929a6d9a66
SGrottel <sgrottel@sgrottel.de>
2022-11-30 21:06:19 +0100

    Second test app to test the nuget

---
+++ TestNugetConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using EverythingSearchClient;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		try
+		{
+			Console.WriteLine("Everything {0}", SearchClient.GetEverythingVersion());
+		}
+		catch (Exception e)
+		{
+			Console.Error.WriteLine("Exception: {0}", e);
+		}
+	}
+}
