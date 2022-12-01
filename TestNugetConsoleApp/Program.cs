86e803466afb51264e8a9a73a47f99f507e47719
SGrottel <sgrottel@sgrottel.de>
2022-12-01 06:41:52 +0100

    Implemented TestNugetConsoleApp

--- TestNugetConsoleApp/Program.cs
+++ TestNugetConsoleApp/Program.cs
@@ -2,19 +2,68 @@
 
 internal class Program
 {
+
+	private static void MyAssert(bool test, string desc)
+	{
+		if (test)
+		{
+			Console.WriteLine("✅ {0}", desc);
+		}
+		else
+		{
+			Console.WriteLine("Failed -- {0}", desc);
+		}
+	}
+
 	private static int Main(string[] args)
 	{
 		try
 		{
+			Console.OutputEncoding = System.Text.Encoding.Unicode;
+
 			Console.WriteLine("Everything {0}", SearchClient.GetEverythingVersion());
 
-			throw new NotImplementedException();
+			string myPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+			Console.WriteLine("Path from Assembly: {0}", myPath);
+
+			string myFileName = Path.GetFileName(myPath);
+			MyAssert(string.Equals(myFileName, "TestNugetConsoleApp.dll", StringComparison.InvariantCultureIgnoreCase), "File name from assembly");
+
+			myPath = Path.GetDirectoryName(myPath) ?? string.Empty;
+			Console.WriteLine("Path: {0}", myPath);
+
+			SearchClient everything = new();
+			Result r = everything.Search(myFileName);
+			Console.WriteLine("Found {0} items", r.NumItems);
+			MyAssert(r.NumItems > 0, "Found files");
+
+			bool found = false;
+			foreach (Result.Item i in r.Items) {
+				Console.WriteLine("{0} / {1}  [{2}]", i.Path, i.Name, i.Flags);
+				if (!string.Equals(i.Name, myFileName, StringComparison.InvariantCultureIgnoreCase))
+				{
+					Console.WriteLine("Warning: file name does not match");
+					continue;
+				}
+				if (!string.Equals(i.Path, myPath, StringComparison.InvariantCultureIgnoreCase))
+				{
+					continue;
+				}
+				if (i.Flags != Result.ItemFlags.None)
+				{
+					Console.WriteLine("Warning: Flags are unexpectedly set");
+				}
+				found = true;
+			}
+			MyAssert(found, "Found my file in the right path");
+
+			throw new Exception("Fail CI on purpose as test");
 
 			return 0;
 		}
 		catch (Exception e)
 		{
-			Console.Error.WriteLine("Exception: {0}", e);
+			Console.Error.WriteLine("❌ Exception: {0}", e);
 			return -1;
 		}
 	}
